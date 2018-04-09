b1e31ce26ced10dea20f08689e844100e057392c
JuanFCrater <juanfbenitezlopez@gmail.com>
2018-04-09 10:28:46 +0200

    Dos semanas de vacaciones y estoy de vuelta, cambios base de datos

--- app/src/main/java/com/craterstudio/juanfrancrater/ipewa/data/db/Contrat/MyContrats.java
+++ app/src/main/java/com/craterstudio/juanfrancrater/ipewa/data/db/Contrat/MyContrats.java
@@ -8,7 +8,7 @@ import android.provider.BaseColumns;
 
 public class MyContrats {
     public static final String DATABASE_NAME = "ipewapp";
-    public static final int DATABASE_VERSION = 6;
+    public static final int DATABASE_VERSION = 7;
 
     public static class Proyectos{
         public static final String TABLE_NAME = "proyecto";
@@ -16,20 +16,22 @@ public class MyContrats {
         public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_COLOR = "color";
         public static final String COLUMN_DEADLINE = "deadline";
-        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE} ;
+        public static final String COLUMN_CREATOR="creator";
+        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_CREATOR} ;
         public static final String DEFAULT_SORT = BaseColumns._ID;
         public static final String SQL_DELETE_ENTRIES = String.format("DROP TABLE IF EXISTS %s",TABLE_NAME);
         public static final String SQL_CREATE_ENTRIES = String.format(
 
-                "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "CREATE TABLE %s (%s TEXT PRIMARY KEY AUTOINCREMENT, " +
 
                         "%s TEXT NOT NULL," +
                         "%s TEXT," +
                         "%s INTEGER NOT NULL,"+
+                        "%s TEXT NOT NULL," +
                         "%s TEXT NOT NULL)",
                 TABLE_NAME,
                 BaseColumns._ID,
-                COLUMN_NAME,COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE);
+                COLUMN_NAME,COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_CREATOR);
 
     }
     public static class Metas{
@@ -40,15 +42,16 @@ public class MyContrats {
         public static final String COLUMN_DEADLINE = "deadline";
         public static final String COLUMN_PRIORITY = "priority";
         public static final String COLUMN_DIFFICULTY = "difficulty";
+        public static final String COLUMN_CREATOR="creator";
         public static final String COLUMN_IDPROYECTO="idProyecto";
 
-        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO} ;
+        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO,COLUMN_CREATOR} ;
         public static final String DEFAULT_SORT = BaseColumns._ID;
 
         public static final String SQL_DELETE_ENTRIES = String.format("DROP TABLE IF EXISTS %s",TABLE_NAME);
         public static final String SQL_CREATE_ENTRIES = String.format(
 
-                "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "CREATE TABLE %s (%s TEXT PRIMARY KEY AUTOINCREMENT, " +
 
                         "%s TEXT NOT NULL," +
                         "%s TEXT," +
@@ -56,11 +59,12 @@ public class MyContrats {
                         "%s TEXT NOT NULL,"+
                         "%s TEXT,"+
                         "%s TEXT," +
-                        "%s INTEGER NOT NULL)",
+                        "%s TEXT NOT NULL," +
+                        "%s TEXT NOT NULL)",
                 TABLE_NAME,
 
                 BaseColumns._ID,
-                COLUMN_NAME,COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO);
+                COLUMN_NAME,COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO,COLUMN_CREATOR);
 
     }
     public static class Actividad{
@@ -102,21 +106,23 @@ public class MyContrats {
         public static final String COLUMN_POSITION = "position";
         public static final String COLUMN_IDPROYECTO="idProyecto";
 
-        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_POSITION,COLUMN_IDPROYECTO} ;
+        public static final String COLUMN_CREATOR="creator";
+        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_POSITION,COLUMN_IDPROYECTO,COLUMN_CREATOR} ;
         public static final String DEFAULT_SORT = COLUMN_POSITION;
 
         public static final String SQL_DELETE_ENTRIES = String.format("DROP TABLE IF EXISTS %s",TABLE_NAME);
         public static final String SQL_CREATE_ENTRIES = String.format(
 
-                "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "CREATE TABLE %s (%s TEXT PRIMARY KEY AUTOINCREMENT, " +
 
                         "%s TEXT NOT NULL," +
                         "%s INTEGER NOT NULL,"+
-                        "%s INTEGER NOT NULL)",
+                        "%s TEXT NOT NULL," +
+                        "%s TEXT NOT NULL)",
                 TABLE_NAME,
 
                 BaseColumns._ID,
-                COLUMN_NAME, COLUMN_POSITION,COLUMN_IDPROYECTO);
+                COLUMN_NAME, COLUMN_POSITION,COLUMN_IDPROYECTO,COLUMN_CREATOR);
 
     }
     public static class Tareas{
@@ -130,13 +136,16 @@ public class MyContrats {
         public static final String COLUMN_IDPROYECTO = "idProyecto";
         public static final String COLUMN_IDTABLERO = "idTablero";
 
-        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO,COLUMN_IDTABLERO} ;
+
+        public static final String COLUMN_CREATOR="creator";
+
+        public static final String[] ALL_COLUMN = {BaseColumns._ID,COLUMN_NAME, COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO,COLUMN_IDTABLERO,COLUMN_CREATOR} ;
         public static final String DEFAULT_SORT = BaseColumns._ID;
 
         public static final String SQL_DELETE_ENTRIES = String.format("DROP TABLE IF EXISTS %s",TABLE_NAME);
         public static final String SQL_CREATE_ENTRIES = String.format(
 
-                "CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "CREATE TABLE %s (%s TEXT PRIMARY KEY AUTOINCREMENT, " +
 
                         "%s TEXT NOT NULL," +
                         "%s TEXT," +
@@ -144,17 +153,18 @@ public class MyContrats {
                         "%s TEXT NOT NULL,"+
                         "%s TEXT,"+
                         "%s TEXT,"+
-                        "%s INTEGER,"+
-                        "%s INTEGER)",
+                        "%s TEXT,"+
+                        "%s TEXT," +
+                        "%s TEXT NOT NULL)",
                 TABLE_NAME,
 
                 BaseColumns._ID,
-                COLUMN_NAME,COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO,COLUMN_IDTABLERO);
+                COLUMN_NAME,COLUMN_DESCRIPTION,COLUMN_COLOR,COLUMN_DEADLINE,COLUMN_PRIORITY,COLUMN_DIFFICULTY,COLUMN_IDPROYECTO,COLUMN_IDTABLERO,COLUMN_CREATOR);
 
 
     }
 
     public static class Usuarios{
-        //Los datos de usuarios en FireBase???
+        //Los datos de usuarios en FireBase->pues si
     }
 }
